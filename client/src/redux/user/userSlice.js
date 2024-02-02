5b6ca772c155f4b6b13a447ae94decc608f44fef
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-02-02 19:53:01 +0530

    add redux persisit

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -22,9 +22,9 @@ const userSlice = createSlice({
         signInFailure: (state,action) => {
             state.loading = false;
             state.error = action.payload;
-        }
-    }
-})
+        },
+    },
+});
 
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
 
