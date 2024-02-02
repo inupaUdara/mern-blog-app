db907480f3101498108ebc99e5482635fcc9e1b1
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-02-02 16:19:01 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req,res,next) => {
     const { username, email, password } = req.body;
 
@@ -19,6 +20,37 @@ export const signup = async (req,res,next) => {
     try {
         await newUser.save();
         res.json("Signup success");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req,res,next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400,"All fields are required"));
+    }
+
+    try {
+        const validateUser = await User.findOne({email})
+        if(!validateUser){
+            return next(errorHandler(400, "User not found"));
+        }
+        const validPassword = bcryptjs.compareSync(password, validateUser.password);
+        if(!validPassword){
+            return next(errorHandler(400, "Invalid password"));
+        }
+
+        const token = jwt.sign({
+            id: validateUser._id
+        },process.env.JWT_SECRET);
+
+        const { password: userPassword, ...rest } = validateUser._doc;
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
     } catch (error) {
         next(error);
     }
